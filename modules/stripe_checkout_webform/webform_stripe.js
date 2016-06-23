492df4e02e469a5862622166b621b04b16fa597d
Laryn - CEDC.org <laryn@cedc.org>
2016-06-23 11:03:02 -0500

    Going to revert and simply rename the files first before committing changes in the hopes it will preserve history on the individual files.
    Revert "Early work to integrate webform_stripe as stripe_checkout_webform"
    
    This reverts commit a459a0bd3e0e2aaf5b43a0a8aa8715de71897b78.

--- modules/stripe_checkout_webform/webform_stripe.js
+++ modules/stripe_checkout_webform/webform_stripe.js
@@ -3,7 +3,7 @@
   /**
    * Scrolls to top of Webform with Stripe payment.
    */
-  Backdrop.ajax.prototype.commands.stripeCheckoutWebformScroll = function(ajax, response, status) {
+  Drupal.ajax.prototype.commands.webformStripeScroll = function(ajax, response, status) {
     // Determine what to scroll to, either our wrapper div, or a block, or a
     // Panels pane.
     var $el = $(response.selector).closest('.block.block-webform');
@@ -27,14 +27,14 @@
   /**
    * Opens Stripe Checkout when Webform Stripe 'pay' button is clicked.
    */
-  Backdrop.behaviors.stripeCheckoutWebform = {
+  Drupal.behaviors.webformStripe = {
     attach: function(context, settings) {
-      $('.stripe-checkout-webform-pay', context).click(function() {
-        StripeCheckout.open($.extend(settings.stripe_checkout_webform, {
+      $('.webform-stripe-pay', context).click(function() {
+        StripeCheckout.open($.extend(settings.webform_stripe, {
           // Payment was successful.
           token: function(token) {
             // Set token and email in token field.
-            $('.stripe-checkout-webform-token', context).val(token.id + ':' + token.email);
+            $('.webform-stripe-token', context).val(token.id + ':' + token.email);
 
             // Submit form.
             $('.webform-submit', context).click();
